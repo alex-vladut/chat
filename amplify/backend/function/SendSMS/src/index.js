a2a2a35dfbe331d184eefccf685c9b3f068bdb17
Alex Vladut <alexandruvladut@gmail.com>
2020-07-01 22:53:59 +0300

    print text message in lambda

--- amplify/backend/function/SendSMS/src/index.js
+++ amplify/backend/function/SendSMS/src/index.js
@@ -1,10 +1,10 @@
-exports.handler = event => {
+exports.handler = (event) => {
   //eslint-disable-line
   console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach(record => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-  });
-  return Promise.resolve('Successfully processed DynamoDB record');
+  event.Records.filter((record) => record.eventName === "INSERT").forEach(
+    (record) => {
+      console.log("DynamoDB Record: %j", record.dynamodb.NewImage.text);
+    }
+  );
+  return Promise.resolve("Successfully processed DynamoDB record");
 };
