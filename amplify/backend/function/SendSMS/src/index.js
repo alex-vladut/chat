bf31cbe8f0140b3d708d831c6d83da09add1f4d3
Alex Vladut <alexandruvladut@gmail.com>
2020-07-01 23:25:52 +0300

    send sms message from lambda

--- amplify/backend/function/SendSMS/src/index.js
+++ amplify/backend/function/SendSMS/src/index.js
@@ -1,9 +1,48 @@
+const AWS = require("aws-sdk");
+const pinpoint = new AWS.Pinpoint();
+
 exports.handler = (event) => {
-  //eslint-disable-line
+  const applicationId = "725ed269bb71427797d4eee66b4e2919";
+  const messageType = "TRANSACTIONAL";
+
   console.log(JSON.stringify(event, null, 2));
   event.Records.filter((record) => record.eventName === "INSERT").forEach(
     (record) => {
-      console.log("DynamoDB Record: %j", record.dynamodb.NewImage.text);
+      const message = record.dynamodb.NewImage.text;
+      const originationNumber = record.dynamodb.NewImage.from;
+      const destinationNumber = record.dynamodb.NewImage.to;
+
+      const params = {
+        ApplicationId: applicationId,
+        MessageRequest: {
+          Addresses: {
+            [destinationNumber]: {
+              ChannelType: "SMS",
+            },
+          },
+          MessageConfiguration: {
+            SMSMessage: {
+              Body: message,
+              MessageType: messageType,
+              OriginationNumber: originationNumber,
+            },
+          },
+        },
+      };
+      pinpoint.sendMessages(params, function (err, data) {
+        // If something goes wrong, print an error message.
+        if (err) {
+          console.log(err.message);
+          // Otherwise, show the unique ID for the message.
+        } else {
+          console.log(
+            "Message sent! " +
+              data["MessageResponse"]["Result"][destinationNumber][
+                "StatusMessage"
+              ]
+          );
+        }
+      });
     }
   );
   return Promise.resolve("Successfully processed DynamoDB record");
