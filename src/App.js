e29523640a58128faf0f422a4bb7a1786702da06
Alex Vladut <alexandruvladut@gmail.com>
2020-07-01 10:32:01 +0300

    add messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { API, graphqlOperation } from "aws-amplify";
+import * as mutations from "./graphql/mutations";
+import * as queries from "./graphql/queries";
 
 function App() {
-  return <div className="App"></div>;
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+
+  const fetchMessages = () => {
+    API.graphql(graphqlOperation(queries.listMessages))
+      .then((response) => setMessages(response.data.listMessages.items))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    API.graphql(graphqlOperation(mutations.createMessage, { input: { text } }))
+      .then(() => {
+        setText("");
+        fetchMessages();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <form onSubmit={submit} className="App">
+      <input value={text} onChange={handleTextChange} />
+      <button type="submit">Save</button>
+      {messages.map((message) => (
+        <p key={message.id}>{message.text}</p>
+      ))}
+    </form>
+  );
 }
 
 export default withAuthenticator(App);
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      text
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      text
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      text
+      createdAt
+      updatedAt
+    }
+  }
+`;
