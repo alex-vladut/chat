db72266de01d37892308e46fceb8afcd7c05ba80
Alex Vladut <alexandruvladut@gmail.com>
2020-07-02 22:19:05 +0300

    add sqs trigger for lambda function

--- amplify/backend/function/ReceiveSms/src/index.js
+++ amplify/backend/function/ReceiveSms/src/index.js
@@ -1,10 +1,8 @@
-
-
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  console.log(event);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
