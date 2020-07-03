eb5436693c16188d7a24f5a09801bd6d046afc2b
Alex Vladut <alexandruvladut@gmail.com>
2020-07-03 22:41:07 +0300

    create message on sms received
    * add IAM auth method

--- amplify/backend/function/ReceiveSms/src/index.js
+++ amplify/backend/function/ReceiveSms/src/index.js
@@ -1,8 +1,59 @@
+const AWS = require("aws-sdk");
+
+const endpoint = new AWS.Endpoint(
+  process.env.API_CHAT_GRAPHQLAPIENDPOINT
+);
+const credentials = new AWS.EnvironmentCredentials("AWS");
+
+const createMessageMutation = /* GraphQL */ `
+  mutation CreateMessage(
+    $input: CreateMessageInput!
+  ) {
+    createMessage(input: $input) {
+      id
+      text
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
 exports.handler = async (event) => {
-  console.log(event);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
+  if (!event.Records || !event.Records.length) {
+    return "No record found";
+  }
+  const {
+    originationNumber: from,
+    destinationNumber: to,
+    messageBody: text,
+  } = JSON.parse(event.Records[0].body);
+
+  const request = new AWS.HttpRequest(endpoint);
+  request.method = "POST";
+  request.region = process.env.REGION;
+  request.headers["Host"] = endpoint.host;
+  request.headers["Content-Type"] = "multipart/form-data";
+  request.body = JSON.stringify({
+    query: createMessageMutation,
+    variables: { input: { from, to, text } },
+  });
+
+  const signer = new AWS.Signers.V4(request, "appsync", true);
+  signer.addAuthorization(credentials, new Date());
+
+  const result = await new Promise((resolve, reject) => {
+    const send = new AWS.NodeHttpClient();
+    send.handleRequest(
+      request,
+      null,
+      (httpResp) => {
+        let body = "";
+        httpResp.on("data", (chunk) => (body += chunk));
+        httpResp.on("end", () => resolve(body));
+      },
+      (err) => reject(err)
+    );
+  });
+
+  return result;
 };
